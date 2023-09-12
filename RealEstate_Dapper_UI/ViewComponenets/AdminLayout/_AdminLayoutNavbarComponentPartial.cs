44df0c53b1f1cc010d6f30cd5bb444a982d26066
EyyupkocaYusuf <EyyupkocaYusuf@gmail.com>
2023-09-12 22:35:52 +0300

    ADmin Layout islemi tamamlandi

---
+++ RealEstate_Dapper_UI/ViewComponenets/AdminLayout/_AdminLayoutNavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponenets.AdminLayout
+{
+    public class _AdminLayoutNavbarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
