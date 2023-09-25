aeff2b96f2fc33bf241735bfa888bacf00bae82c
EyyupkocaYusuf <EyyupkocaYusuf@gmail.com>
2023-09-25 22:30:11 +0300

    Employe api islemleri icin repolar ve dto lar olusturuldu.

---
+++ RealEstate_Dapper_Api/Repositories/EmployeeRepository/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using RealEstate_Dapper_Api.Dtos.EmployeeDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.EmployeeRepository
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<ResultEmployeeDto>> GetAllEmployeeAsync();
+        void CreateEmployee(CreateEmployeeDto employeeDto);
+        void DeleteEmployee(int id);
+        void UpdateEmployee(UpdateEmployeeDto employeeDto);
+        Task<GetByIDEmployeeDto> GetEmployee(int id);
+    }
+}
