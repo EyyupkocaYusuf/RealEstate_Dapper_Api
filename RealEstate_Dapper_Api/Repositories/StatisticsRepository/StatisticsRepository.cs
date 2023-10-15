4e55bf175735122d936f4455d567cb2fafc03349
EyyupkocaYusuf <EyyupkocaYusuf@gmail.com>
2023-10-15 21:07:31 +0300

    Istatistik alani icin api katmaninda interface ve class olusturuldu. Metotlar tanimlandi

---
+++ RealEstate_Dapper_Api/Repositories/StatisticsRepository/StatisticsRepository.cs
@@ -0,0 +1,89 @@
+﻿using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.StatisticsRepository
+{
+    public class StatisticsRepository : IStatisticsRepository
+    {
+        private readonly Context _context;
+
+        public StatisticsRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public int ActiveCategoryCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ActiveEmployeeCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ApartmentCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal AverageProductByRent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal AverageProductBySale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CategoryCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CategoryNameByMaxProductCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CityNameByMaxProductCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DifferentCityCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string EmployeeNameByMaxProductCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal LastProductPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string NewestBuildingYear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string OldestBuildingYear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PassiveCategoryCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProductCount()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
